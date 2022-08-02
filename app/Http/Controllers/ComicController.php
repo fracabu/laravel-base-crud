74510799b6304083ada7e2b31a55a68571ab9773
fracabu <100199965+fracabu@users.noreply.github.com>
2022-08-02 17:48:00 +0200

    mod

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Comic;
 use Illuminate\Http\Request;
 
 class ComicController extends Controller
@@ -13,7 +14,7 @@ class ComicController extends Controller
      */
     public function index()
     {
-        return "pagina index comics";
+        $data = Comic::all();
     }
 
     /**
